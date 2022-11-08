31171ca7f5487739ac72a1b7a974e54f52d4dd2b
Loren <lorensprague@icloud.com>
2022-11-08 06:01:28 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
 
   get '/movies/:id/summary', to: 'movies#summary'
+  get '/movie_summaries', to: 'movies#summaries'
 end
